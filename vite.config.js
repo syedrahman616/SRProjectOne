cb4551ed27cf018177cd343c59e4fda8971bbf8c
pathirakali <pathirakali@heptotechnologie.org>
2024-01-23 16:19:53 +0530

    .env changes completed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import {defineConfig, loadEnv} from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "./wwwroot/app/",
+      sourcemap: true,
+    },
+    server: {
+      port: env.VITE_PORT,
+    },
+  };
+});
